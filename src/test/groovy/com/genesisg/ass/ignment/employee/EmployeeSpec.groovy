a4032699dac2c9178b995861c558c99446e72ec0
jvman9527 <jvman9527@gmail.com>
2019-11-05 22:13:04 +0800

    add test case about boss

--- src/test/groovy/com/genesisg/ass/ignment/employee/EmployeeSpec.groovy
+++ src/test/groovy/com/genesisg/ass/ignment/employee/EmployeeSpec.groovy
@@ -2,11 +2,11 @@ package com.genesisg.ass.ignment.employee
 
 import spock.lang.Specification
 
-class EmployeeSpec extends Specification{
+class EmployeeSpec extends Specification {
 
     def "fresher is level 1 employee"() {
         expect:
-        new Fresher(" DominicThiem").level == 1
+        new Fresher("DominicThiem").level == 1
     }
 
     def "technical lead is level 5 employee"() {
@@ -17,6 +17,26 @@ class EmployeeSpec extends Specification{
     def "project manager is level 10 employee"() {
         expect:
         new ProjectManager("RafaelNadal").level == 10
+    }
+
+    def "boss is level 100, and he has a powerful solution"() {
+        given:
+        def boss = new Boss("Djoko")
+
+        expect:
+        assert boss.level == 100
+        assert boss.solution instanceof AllFreeSolution
+    }
+
+    def "boss is always free"() {
+        given:
+        def boss = new Boss("Djoko")
+
+        when:
+        boss.free = false
+
+        then:
+        boss.free
     }
 
 }
